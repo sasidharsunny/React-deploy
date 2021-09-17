548e3872a675fc2f959a2df724b8e0bddda47c87
root <root@ip-172-31-40-160.ap-south-1.compute.internal>
2021-09-17 07:50:12 +0000

    modified react code

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+            <h1>this is react deployement code by sasi<h1>
         </p>
         <a
           className="App-link"
